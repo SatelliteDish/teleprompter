c0294724930b9f32453101c74024aaecd9caee5a
LarsenEndeavors <larsenendeavors@gmail.com>
2025-12-10 01:31:38 -0600

    Updated Some Configurations and killed a comment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false, // <-- Turn this to false for Voice Apps
 };
 
 export default nextConfig;
